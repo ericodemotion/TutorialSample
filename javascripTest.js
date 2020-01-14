f44b1d5eac38ef1473938eeafbc73873103ce308
codEmotion <codemotion@codEmotions-MacBook-Pro.local>
2020-01-14 14:28:39 +0100

    test examples

--- javascripTest.js
+++ javascripTest.js
@@ -1,4 +1,14 @@
 console.log("test");
-console.log("gogogo")
+console.log("gogogo");
+console.log("gogogo");
+console.log("gogGOGOogo");
+console.log("gogogo");
+console.log("gogogogo");
 
-// Developer with Talent and Soft Skills :-)alent and Soft Skills ...
+console.log("gogogo");
+console.log("gogogo");
+console.log("goggogogoogo");
+console.log("gogogogsghhghggo");
+console.log("gogogo");
+
+// Developer with Talent and Soft Skills :-)alent and Soft Skills ..
